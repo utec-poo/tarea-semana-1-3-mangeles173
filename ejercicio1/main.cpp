afdbe7596e37fac5b7ebb50d3b9f5bbdd672fdd9
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-13 16:16:53 -0500

    Update main.cpp

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-#include "Tipos.h"
+
 using namespace std;
 
 int main() {
 
-int base=0, int altura=0;
+double base, altura, area;
 cout << "Ingrese la base: \n";
 cin >> base;
 cout << "Ingrese la altura: \n";
 cin >> altura;
 
-
-// ToDO
-
+if (base<0|| altura <0)
+cout << "No existe un triángulo con lados negativos\n";
+else{
+area = base * altura /2.0;
+cout <<"El area es: "<<area<<"\n";
+}
   return 0;
 }
