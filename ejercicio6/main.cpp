4d981fd82084e9ff4aba52d9843292447140fa1e
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-15 13:25:38 -0500

    Update main.cpp

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
-#include "Tipos.h"
+
 using namespace std;
-
 int main() {
+  int num,dig1,dig2, dig3,i=0;
+  int par=0, impar=0;
 
-entero numero;
+  
+  cout << "Ingrese un numero de 3 dígitos: \n";
+  cin >> num;
+  dig3=num%10;
+  dig2=(num/10)%10;
+  dig1=(num/10)/10;
 
-
-cout << "ingrese un numero:";
-
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
+  if (dig1==0){
+    cout<<"Debe ingresar un numero de 3 dígitos\n";
+  }
+  else{
+    if (dig1!=0){
+      if (dig1%2==0)
+        par++;
+      else
+        impar++;
+    }
+    if (dig2!=-1){
+      if (dig2%2==0)
+        par++;
+      else
+        impar++;
+    }
+    if (dig3!=-1){
+      if (dig3%2==0)
+        par++;
+      else
+        impar++;
+    }
+    cout<<"El numero tiene 3 dígitos, de los cuales "<<par<<" son pares y "<<impar<<" son impares.\n";
+  }
+ 
   return 0;
 }
