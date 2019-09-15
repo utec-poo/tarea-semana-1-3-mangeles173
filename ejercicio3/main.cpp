3d6e1d78dd1b2599c89d31c96dadedff9ce5d757
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-15 11:10:47 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
+int main(){
+  char letra;
+  int numero;
 
-entero numero;
-caracter crt;
+  cout << "Ingrese Cuadrante: \n"; cin >> letra >> numero;
 
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
+  if (numero >= 1 && numero <=8 && letra >= 'a' && letra <= 'h' ){
+    if (letra == 'a' || letra == 'c' || letra == 'e' || letra == 'g'){
+      if (numero%2==0){
+        cout << "Espacios blancos \n";
+      }
+      else{
+        cout << "Espacios negros \n";
+      }
+    }
+    else{
+      if (numero%2==0){
+        cout << "Espacios negros \n";
+      }        
+      else{ 
+        cout << "Espacios blancos \n";
+      }
+    }  
+  }
+  else{
+    cout << "El cuadrante " << letra << numero << " no existe \n";
+  }
   return 0;
 }
