fc18cd3c834b117ad7d44c0f2015e395ba59cc44
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-15 20:20:15 -0500

    Update main.cpp

--- ejercicio7/main.cpp
+++ ejercicio7/main.cpp
@@ -1,13 +1,22 @@
+//**Escribir un programa en C++, que permita leer un número entre 10 y 30 e imprima tantos * como indica el número. El programa debe verificar que el número que se ingresa se encuentra en el rango de 10 y 30 inclusive.
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
 int main() {
+  int num=0;
+  int i=1;
 
-entero numero;
-cin >> numero;
+  cout <<"Ingrese un numero\n";
+  cin>>num;
 
-// ToDO
-// implemente el algoritmo.
+  if (num<10 || num>30){
+    cout<<"Numero fuera del rango\n";
+    cin>>num;
+  }
+  
+  for (int i=1; i<=num; i++){
+    cout << "*";
+  }
+     
   return 0;
 }
