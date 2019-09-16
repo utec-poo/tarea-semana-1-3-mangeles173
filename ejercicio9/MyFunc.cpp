6e11508c2de1da776db70cada7c15e4e23cbdc44
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-15 22:23:03 -0500

    Update MyFunc.cpp

--- ejercicio9/MyFunc.cpp
+++ ejercicio9/MyFunc.cpp
@@ -1,10 +1,17 @@
 #include "MyFunc.h"
 
+Tipo_double Convertirhor(Tipo_double num,Tipo_double hor){
+  hor=num/3600.0;
+  return hor;
 
-int invertir(int n){
-// Escriba su código aqui
 }
 
-bool es_capicua(int n){
-// Escriba su código aqu
+Tipo_double Convertirmin(Tipo_double num,Tipo_double min, Tipo_double hor){
+  min=(num-hor*3600.0)/60;
+  return min;
+}
+
+Tipo_double Convertirseg(Tipo_double num,Tipo_double seg, Tipo_double hor, Tipo_double min){
+  seg=num-(hor*3600.0+min*60.0);
+  return seg;
 }
