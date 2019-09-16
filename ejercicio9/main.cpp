03af35f92a050b96b6235f38e5499e34194dfa8e
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-15 22:23:34 -0500

    Update main.cpp

--- ejercicio9/main.cpp
+++ ejercicio9/main.cpp
@@ -1,16 +1,23 @@
+//**Desarrolle una función que reciba como parámetro una cantidad de segundos y realice la conversión a su equivalente en horas, minutos y segundos
+
 #include <iostream>
-#include "tipos.h"
 #include "MyFunc.h"
+#include "MyFunc.cpp"
+
 using namespace std;
+
 int main() {
-    l_enteros segundos;
-    enteros horas, min, seg;
+  Tipo_double num,seg, min, hor;
 
-    do {
-        cout << "Segundos: ";
-        cin >> segundos;
-    } while (segundos <1);
+  do {
+    cout << "Cantidad total de segundos: ";
+    cin >> num;
+  } while (num <1.0);
 
-    // ToDO
-    return 0;
+ 
+  cout <<  "Se tienen "<<Convertirhor(num,hor)<< "horas\n";
+  cout <<  "Se tienen "<<Convertirmin(num,hor,min)<< "minutos\n";
+  cout <<  "Se tienen "<<Convertirseg(num,hor,min,seg)<<"segundos\n";
+
+  return 0 ;
 }
