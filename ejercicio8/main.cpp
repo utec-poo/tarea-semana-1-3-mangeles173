6ec5b78da266fc828283e9fd080308cd891886f6
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-15 20:40:13 -0500

    Update main.cpp

--- ejercicio8/main.cpp
+++ ejercicio8/main.cpp
@@ -1,15 +1,23 @@
-#include <iostream>
-#include "Tipos.h"
+//**leer un número entre 1 y 30 e imprima un triángulo con asteriscos. El triángulo tendrá tantas filas como indica el número.
+
+#include<iostream>
 using namespace std;
+ 
+int main()
+{
+	int i;
+	int valor;
+ 
+	cout<<"introduzca valor (1-30)\n";
+	cin>>valor;
 
-int main() {
-
-entero numero;
-
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
+  if (valor<1 || valor>30){
+    cout<<"Numero fuera del rango\n";
+  }
+  for (int i=1; i<=valor; i++){
+    for(int a=1; i>=a; a++)
+      cout<<"*";
+      cout<<"\n";
+  }
   return 0;
 }
