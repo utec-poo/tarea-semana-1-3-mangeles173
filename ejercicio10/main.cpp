3c6ed3cbed0c7fec8ceecb76808ece2a219da4cd
mangeles173 <55298307+mangeles173@users.noreply.github.com>
2019-09-18 18:33:33 -0500

    Update main.cpp

--- ejercicio10/main.cpp
+++ ejercicio10/main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
+typedef long int tipo_entero;
+tipo_entero LeeNumero();
+tipo_entero Inverso(tipo_entero);
+
+tipo_entero LeeNumero(){
+  tipo_entero n=0;
+  do{
+    cout<<"Ingrese un número de 7 dígitos: \n";
+    cin>>n;
+  }while (n<1000000);
+  return n;
+  
+}
+tipo_entero Inverso(tipo_entero numero){
+  tipo_entero Inverso=0, d;
+  while (numero>0){
+    d=numero%10;
+    Inverso=Inverso*10+d; 
+    numero=numero/10;
+  }
+return Inverso;
+}
+
 int main() {
+  tipo_entero numero;
+  numero=LeeNumero();
+  if (numero==Inverso(numero)){
+    cout<<"Es un número capicúa\n";
+  }
+  else{
+    cout <<"No es un número capicúa\n";
+  }
 
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
   return 0;
 }
